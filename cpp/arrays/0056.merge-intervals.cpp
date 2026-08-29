3702c8784798475b202f89cfda306bdb716139cb
Kishore-Dev <kishoreprakash45@gmail.com>
2026-08-29 12:33:21 +0530

    Implement merge intervals function in C++

--- cpp/arrays/0056.merge-intervals.cpp
+++ cpp/arrays/0056.merge-intervals.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        
+        if(intervals.empty()) 
+            return result;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<int> current = intervals[0];
+        
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= current[1]) {
+                current[1] = max(current[1], intervals[i][1]);
+            } 
+            else {
+                result.push_back(current);
+                current = intervals[i];
+            }
+        }
+        
+        result.push_back(current);
+        
+        return result;
+    }
+};
