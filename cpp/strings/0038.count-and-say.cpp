64edc6ceff12683f65fe88e8604c25f97b0aeb5c
Kishore-Dev <kishoreprakash45@gmail.com>
2026-09-06 21:04:44 +0530

    Update 0038.count-and-say.cpp

--- cpp/strings/0038.count-and-say.cpp
+++ cpp/strings/0038.count-and-say.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1)
+            return "1";
 
+        string RLE = countAndSay(n - 1);
+
+        string newRLE = "";
+        int count = 1;
+
+        for (int i = 1; i < RLE.size(); i++) {
+            if (RLE[i - 1] == RLE[i]) {
+                count++;
+            } else {
+                newRLE += to_string(count) + RLE[i - 1];
+                count = 1;
+            }
+        }
+
+        newRLE += to_string(count) + RLE[RLE.size() - 1];
+
+        return newRLE;
+    }
+};
