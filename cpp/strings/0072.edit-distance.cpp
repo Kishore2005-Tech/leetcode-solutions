9e0918ed87272f7b3e6f44d722cfa1a7019c7fe2
Kishore-Dev <kishoreprakash45@gmail.com>
2026-09-06 21:19:54 +0530

    Implement edit distance calculation

--- cpp/strings/0072.edit-distance.cpp
+++ cpp/strings/0072.edit-distance.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
 
+        vector<int> dp(m + 1, INT_MAX);
+
+        for (int i = 0; i <= m; i++) {
+            dp[i] = m - i;
+        }
+
+        int curr = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int prevDiag = dp[m];
+            dp[m] = curr;
+
+            for (int j = m - 1; j >= 0; j--) {
+                int temp = dp[j];
+
+                if (word1[i] != word2[j]) {
+                    dp[j] = min({
+                        1 + temp,      // delete
+                        1 + dp[j + 1], // insert
+                        1 + prevDiag   // replace
+                    });
+                }
+                else {
+                    dp[j] = prevDiag;
+                }
+
+                prevDiag = temp;
+            }
+
+            curr++;
+        }
+
+        return dp[0];
+    }
+};
