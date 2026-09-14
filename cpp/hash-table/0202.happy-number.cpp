3040d314dfa63105a07ad273fa7b1c5084786067
Kishore-Dev <kishoreprakash45@gmail.com>
2026-09-14 21:14:12 +0530

    Implement happy number algorithm in C++

--- cpp/hash-table/0202.happy-number.cpp
+++ cpp/hash-table/0202.happy-number.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
 
+        while (n != 1) {
+
+            // Number appeared before → cycle
+            if (seen.count(n))
+                return false;
+
+            seen.insert(n);
+
+            int sum = 0;
+
+            // Calculate sum of squared digits
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+};
