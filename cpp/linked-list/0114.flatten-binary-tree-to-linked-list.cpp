eb8b1c9a39461ec06f43a34a31b1801e53b250b7
Kishore-Dev <kishoreprakash45@gmail.com>
2026-09-22 22:37:55 +0530

    Add flatten function for binary tree
    Implement a solution to flatten a binary tree to a linked list using a helper function.

--- cpp/linked-list/0114.flatten-binary-tree-to-linked-list.cpp
+++ cpp/linked-list/0114.flatten-binary-tree-to-linked-list.cpp
@@ -1,1 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* temp = nullptr;
 
+public:
+    void flatten(TreeNode* root) {
+        helper(root);
+    }
+
+    void helper(TreeNode* root) {
+        if (root == nullptr)
+            return;
+
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+
+        if (temp != nullptr) {
+            temp->right = root;
+        }
+
+        root->left = nullptr;
+        temp = root;
+
+        helper(left);
+        helper(right);
+    }
+};
